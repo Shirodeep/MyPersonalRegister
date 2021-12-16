41df43fd377e3e7c676fc9d9edd1e0a28b74f14b
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-16 17:42:06 +0545

    .\main.dart

--- register/lib/main.dart
+++ register/lib/main.dart
@@ -57,10 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      if (_counter > 0)
-        _counter--;
-      else
-        _counter = 0;
+      _counter++;
     });
   }
 
