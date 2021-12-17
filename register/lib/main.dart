26eead6e6036c5a8198288efa6540f107f3e71f6
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-17 23:41:18 +0545

    This is for today

--- register/lib/main.dart
+++ register/lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Register"),
-        ),
-        body: Center(
-          child: Text("Hello ninjas"),
-        ),
-      ),
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My Register",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.orange[100],
+      ),
+      body: Center(
+        child: Text('FOR THE VIEW WELCOME'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+      ),
+    );
+  }
+}
