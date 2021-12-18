779c5f26601a20dab386cf167f69184f5e5dc28a
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-18 15:47:42 +0545

    jajaj

--- register/lib/main.dart
+++ register/lib/main.dart
@@ -19,8 +19,10 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.orange[100],
       ),
       body: Center(
-        child: Text('FOR THE VIEW WELCOME'),
-      ),
+          child: Image(
+        image: NetworkImage(
+            'https://scontent.fktm12-1.fna.fbcdn.net/v/t39.30808-6/s600x600/267557745_206935594970928_1910818178901165820_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=dKDdd3jBWbsAX9hKIYT&_nc_ht=scontent.fktm12-1.fna&oh=00_AT9vEpXdsAckKaIZhfKwQLnIEyKgEG0idTdTZu6ZXqdCaw&oe=61C2BB0B'),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
