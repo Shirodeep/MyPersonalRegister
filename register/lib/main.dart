f20b22f513338a602544fdff7d0a2438e4c47476
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-18 23:05:14 +0545

    Updated

--- register/lib/main.dart
+++ register/lib/main.dart
@@ -19,36 +19,47 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.orange[100],
         ),
         body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text('Hello Programmers'),
                   FlatButton(
                     child: Text('Click me'),
                     onPressed: () => print('Hello'),
+                    color: Colors.grey,
                   ),
                 ],
               ),
             ),
             Container(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text('Hello Programmers'),
                   FlatButton(
                     child: Text('Click me'),
                     onPressed: () => print('Hello'),
+                    color: Colors.red,
                   ),
                 ],
               ),
             ),
             Container(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Text('Hello Programmers'),
                   FlatButton(
                     child: Text('Click me'),
                     onPressed: () => print('Hello'),
+                    color: Colors.blue,
                   ),
                 ],
               ),
