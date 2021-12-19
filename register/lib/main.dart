6dc30006b73a548383c134ac0d3f432c8256b4e3
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-19 21:44:32 +0545

    .

--- register/lib/main.dart
+++ register/lib/main.dart
@@ -4,67 +4,52 @@ void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
+int onTapFunction() {
+  print('Sanjay');
+}
+
 class Home extends StatelessWidget {
+  var _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "My Register",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          "My Register",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
           ),
-          backgroundColor: Colors.orange[100],
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Hello Programmers'),
-                  FlatButton(
-                    child: Text('Click me'),
-                    onPressed: () => print('Hello'),
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('Hello Programmers'),
-                  FlatButton(
-                    child: Text('Click me'),
-                    onPressed: () => print('Mister'),
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text('Hello Programmers'),
-                  FlatButton(
-                    child: Text('Click me'),
-                    onPressed: () => print('Shirodeep'),
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ));
+        backgroundColor: Colors.orange[100],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_fields),
+            label: 'Blank Text',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.read_more),
+            label: 'Ledger',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Blogs',
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.privacy_tip),
+            label: 'Private',
+            backgroundColor: Colors.blueGrey,
+          ),
+        ],
+        onTap: onTapFunction(),
+      ),
+    );
   }
 }
