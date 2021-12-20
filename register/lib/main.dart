e614d1cdbffdd0faa1bd68a52cd41861ccaa09d6
Shirodeep <shirodeepdahal0987@gmail.com>
2021-12-20 22:34:13 +0545

    Todays Work

--- register/lib/main.dart
+++ register/lib/main.dart
@@ -4,8 +4,14 @@ void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
-class Home extends StatelessWidget {
-  var _currentIndex = 0;
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final Tabs = [Center()];
 
   @override
   Widget build(BuildContext context) {
@@ -20,36 +26,41 @@ class Home extends StatelessWidget {
         ),
         backgroundColor: Colors.orange[100],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.text_fields),
-            label: 'Blank Text',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.read_more),
-            label: 'Ledger',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Blogs',
-            backgroundColor: Colors.pink,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.privacy_tip),
-            label: 'Private',
-            backgroundColor: Colors.blueGrey,
-          ),
-        ],
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
+      body: Center(
+        child: Container(
+          child: Text('LALA'),
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.text_fields),
+              label: 'Blank Text',
+              backgroundColor: Colors.blue,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.read_more),
+              label: 'Ledger',
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Blogs',
+              backgroundColor: Colors.pink,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.privacy_tip),
+              label: 'Private',
+              backgroundColor: Colors.blueGrey,
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          }),
     );
   }
 }
